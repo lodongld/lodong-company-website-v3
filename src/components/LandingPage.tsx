c8bf133524f676bb4fb0cace753c336a8b4e4ed5
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2023-04-24 19:45:53 +0800

    increased font sizes for banner text

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import Photo from "./../img/team-looking-busy.jpg";
 import { motion } from "framer-motion";
+import css from "./../styles/landing.module.css";
 
 export const LandingPage: FunctionComponent = (): JSX.Element => {
   const styles = {
@@ -48,16 +49,16 @@ export const LandingPage: FunctionComponent = (): JSX.Element => {
             }}
             className="flex flex-col justify-center row-span-1 row-start-2 text-end gap-y-3"
           >
-            <div className="text-4xl text-white">실패 경험 <span className="font-bold">ZERO</span></div>
-            <div className="text-4xl text-white">세상에 없던 디지털 솔루션 개발 전문</div>
-            <div className="text-9xl font-bold text-[#FA2B2B]">LODONG</div>
+            <div className={css.header}>실패 경험 <span className="font-extrabold">ZERO</span></div>
+            <div className={css.header}>세상에 없던 디지털 솔루션 개발 전문</div>
+            <div className={css.lodong}>LODONG</div>
           </motion.div>
         </div>
 
-        <motion.div initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className="flex flex-col justify-center row-span-1 row-start-3 px-10 bg-opacity-50 lg:px-16 gap-y-3 bg-neutral-900">
-          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className="font-bold text-white lg:text-4xl">We Develop Innovative</motion.h1>
-          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className="font-bold text-gray-500 lg:text-4xl">Digital Solutions</motion.h1>
-          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className="font-bold text-white lg:text-4xl">Enhancing User Experience</motion.h1>
+        <motion.div initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className="row-span-1 row-start-3 px-10 bg-opacity-50 lg:px-16 bg-neutral-900">
+          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className={css.headerText}>We Develop Innovative</motion.h1>
+          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className={css.headerText}>Digital Solutions</motion.h1>
+          <motion.h1 initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 2.5, duration: 2 }} className={css.headerText}>Enhancing User Experience</motion.h1>
         </motion.div>
       </div>
     </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: FunctionComponent = (): JSX.Element => {
     <Fragment>
       <Head>
         <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Lodong</title>
       </Head>
