09fe9685cebb9324ab26a49036509d154cadc8ed
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2023-04-26 15:29:36 +0800

    added doc comments for animated components

--- src/ts/Motion.tsx
+++ src/ts/Motion.tsx
@@ -9,10 +9,16 @@ interface AnimatedProps {
   }
 }
 
+/**
+ * @description An animated &lt;p&gt; component.
+ */
 export const Paragraph: FunctionComponent<AnimatedProps> = (props): JSX.Element => {
   return (<motion.p className={props.className}>{props.children}</motion.p>);
 };
 
+/**
+ * @description An animated &lt;div&gt; component.
+ */
 export const Division: FunctionComponent<AnimatedProps> = (props) => {
   return (
     <motion.div
@@ -38,6 +44,9 @@ export const Division: FunctionComponent<AnimatedProps> = (props) => {
   );
 };
 
+/**
+ * @description An animated &lt;h1&gt; component.
+ */
 export const Heading1: FunctionComponent<AnimatedProps> = (props) => {
   return (
     <motion.h1
@@ -51,21 +60,37 @@ export const Heading1: FunctionComponent<AnimatedProps> = (props) => {
   );
 };
 
+/**
+ * @description An animated &lt;h2&gt; component.
+ */
 export const Heading2: FunctionComponent<AnimatedProps> = (props) => {
   return (<motion.h2 className={props.className} style={props.style}>{props.children}</motion.h2>);
 };
 
+/**
+ * @description An animated &lt;h3&gt; component.
+ */
 export const Heading3: FunctionComponent<AnimatedProps> = (props) => {
   return (<motion.h3 className={props.className} style={props.style}>{props.children}</motion.h3>);
 };
 
+/**
+ * @description An animated &lt;h4&gt; component.
+ */
 export const Heading4: FunctionComponent<AnimatedProps> = (props) => {
   return (<motion.h4 className={props.className} style={props.style}>{props.children}</motion.h4>);
 };
 
+/**
+ * @description An animated &lt;h5&gt; component.
+ */
 export const Heading5: FunctionComponent<AnimatedProps> = (props) => {
   return (<motion.h5 className={props.className} style={props.style}>{props.children}</motion.h5>);
 };
+
+/**
+ * @description An animated &lt;h6&gt; component.
+ */
 export const Heading6: FunctionComponent<AnimatedProps> = (props) => {
   return (<motion.h6 className={props.className} style={props.style}>{props.children}</motion.h6>);
 };
